73f0bc4b24c00ecc0bdd060bfdb95760842832de
Victor Hugo Novais <victor.novais@sieve.com.br>
2016-02-20 18:11:41 -0200

    Refactoring class name

--- classifier.py
+++ classifier.py
@@ -8,7 +8,7 @@ import connections
 import os
 
 
-class SentimentAnalyzer(object):
+class SentimentClassifier(object):
     def __init__(self):
         """
         Constructs a new SentimentAnalyzer instance.
@@ -18,23 +18,10 @@ class SentimentAnalyzer(object):
         self.data = self.get_tweets_data(collection)
         self.stopSet = set(stopwords.words('english'))
 
-        # Naive Bayes initialization
-        self._init_naive_bayes()
-
     def get_tweets_data(self, hashtag):
         with connections.get_db_connection() as client:
             tweets = client['tweets'][hashtag].find({'lang': 'en', 'retweeted': False})
             return tweets
-
-    def _init_naive_bayes(self):
-        """
-        _init_naive_bayes(self):
-        Gets the data from the positive, negative and neutral text files.
-        Creates and trains the Naive Bayes classifier, using the data, so
-        that it can learn what constitutes a positive, negative or neutral tweet.
-        """
-
-        pass
 
     def classify(self):
         for tweet in self.data[:12]:
