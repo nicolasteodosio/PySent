cf8e13998a13cefe1e79d72fbdbc96ed466858fe
Nicolas Teodosio <nicolas.steodosio@gmail.com>
2016-03-06 16:38:12 -0300

    Creating normalized wordbase from SentiLex

---
+++ wordbase_normalizer.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import os
+
+
+def wordbase_normalizer():
+    txt_file = open(os.path.abspath('word_database/SentiLex-PT02/SentiLex-lem-PT02.txt'), 'r')
+    new_pos_file = open(os.path.abspath('word_database/SentiLex-PT02/normalized/positive/pos.txt'), 'w')
+    new_neg_file = open(os.path.abspath('word_database/SentiLex-PT02/normalized/negative/neg.txt'), 'w')
+    new_neut_file = open(os.path.abspath('word_database/SentiLex-PT02/normalized/neutral/net.txt'), 'w')
+    for line in txt_file:
+        line_split = line.split(';')
+        polarity = line_split[2].split('=')[1]
+        word = line_split[0].split('.')[0]
+        if polarity == '1':
+            new_pos_file.write(word + '.\n')
+        elif polarity == '-1':
+            new_neg_file.write(word + '.\n')
+        else:
+            new_neut_file.write(word + '.\n')
