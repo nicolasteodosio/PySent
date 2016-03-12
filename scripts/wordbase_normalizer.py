758fc65ce24c4aabe7be1a81202cd092179542f1
Nicolas Teodosio <nicolas.steodosio@gmail.com>
2016-03-12 12:38:40 -0300

    Addinf script to format emoticons

--- scripts/wordbase_normalizer.py
+++ scripts/wordbase_normalizer.py
@@ -8,7 +8,7 @@ no padrão necessáio pelo textblob
 import os
 
 
-def wordbase_normalizer():
+def wordbase_normalizer_sentilex():
     txt_file = open(os.path.abspath('word_database/SentiLex-PT02/SentiLex-lem-PT02.txt'), 'r')
     new_pos_file = open(os.path.abspath('word_database/SentiLex-PT02/normalized/positive/pos.txt'), 'w')
     new_neg_file = open(os.path.abspath('word_database/SentiLex-PT02/normalized/negative/neg.txt'), 'w')
@@ -23,3 +23,20 @@ def wordbase_normalizer():
             new_neg_file.write(word + '.\n')
         else:
             new_neut_file.write(word + '.\n')
+
+
+def wordbase_normalizer_emoticon_sentiment_lexicon():
+    txt_file = open(os.path.abspath('word_database/emoticon_base/EmoticonSentimentLexicon.txt'), 'r')
+    new_pos_file = open(os.path.abspath('word_database/emoticon_base/positive/pos.txt'), 'w')
+    new_neg_file = open(os.path.abspath('word_database/emoticon_base/negative/neg.txt'), 'w')
+    new_neut_file = open(os.path.abspath('word_database/emoticon_base/neutral/net.txt'), 'w')
+    for line in txt_file:
+        line_split = line.split(',,')
+        polarity = line_split[1]
+        word = line_split[0]
+        if polarity == '1\r\n':
+            new_pos_file.write(word + '\n')
+        elif polarity == '-1\r\n':
+            new_neg_file.write(word + '\n')
+        else:
+            new_neut_file.write(word + '\n')
