cfb7e7eb003b190d4ec71048411cc123352e6d0c
Victor Hugo Novais <victor.novais@sieve.com.br>
2016-03-12 12:13:27 -0300

    Setting a standart for database files

--- classifier.py
+++ classifier.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import nltk
 from nltk.corpus import stopwords
 
 from unidecode import unidecode
@@ -27,3 +28,27 @@ class SentimentClassifier(object):
         for tweet in self.data[:12]:
             blob = TextBlob(tweet['text'], analyzer=NaiveBayesAnalyzer())
             print 'Tweet: {} || analisys: {}'.format(unidecode(tweet['text']), blob.sentiment)
+
+
+class CustomNaiveBayesAnalyzer(NaiveBayesAnalyzer):
+    def _default_feature_extractor(words):
+        """Default feature extractor for the NaiveBayesAnalyzer."""
+        return dict(((word, True) for word in words))
+
+    def __init__(self, feature_extractor=_default_feature_extractor, corpus=None):
+        super(NaiveBayesAnalyzer, self).__init__()
+        self._classifier = None
+        self.feature_extractor = feature_extractor
+
+    def train(self):
+        """Train the Naive Bayes classifier on the movie review corpus."""
+        super(NaiveBayesAnalyzer, self).train()
+        import ipdb; ipdb.set_trace()
+        neg_ids = nltk.corpus.movie_reviews.fileids('neg')
+        pos_ids = nltk.corpus.movie_reviews.fileids('pos')
+        neg_feats = [(self.feature_extractor(
+            nltk.corpus.movie_reviews.words(fileids=[f])), 'neg') for f in neg_ids]
+        pos_feats = [(self.feature_extractor(
+            nltk.corpus.movie_reviews.words(fileids=[f])), 'pos') for f in pos_ids]
+        train_data = neg_feats + pos_feats
+        self._classifier = nltk.classify.NaiveBayesClassifier.train(train_data)
