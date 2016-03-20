e523c6d11b02e3029f9e1cd7a5da79dd2df3649a
Victor Hugo Novais <victor.novais@sieve.com.br>
2016-03-20 14:50:04 -0300

    Creating tweet normalizer that can filter stopwords from nltk and twitter

---
+++ normalizer.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+from nltk import SnowballStemmer
+from nltk.corpus import stopwords
+
+
+TWITTER_STOPWORDS = [
+    '@',
+    'RT',
+    'http'
+]
+
+
+class TweetNormalizer(object):
+    def __init__(self, language):
+        self.language = language
+        self.stemmer = SnowballStemmer(language, ignore_stopwords=True)
+
+    def clean_stopwords(self, text):
+        # Cleaning portuguese stopwords
+        splitted = [i for i in text.split() if i not in stopwords.words(self.language)]
+        cleaned_splitted = []
+
+        # Cleaning twitter stopwords
+        for word in splitted:
+            cleaned_splitted.append(word)
+
+            for twitter_stopword in TWITTER_STOPWORDS:
+                if word.startswith(twitter_stopword):
+                    cleaned_splitted.remove(word)
+
+        return ' '.join(cleaned_splitted)
+
+    def stem(self, text):
+        splitted = text.split()
+
+        for i, word in enumerate(splitted):
+            stem_word = self.stemmer.stem(word)
+            splitted[i] = stem_word
+
+        return ' '.join(splitted)
+
+    def normalize(self, text):
+        text = self.clean_stopwords(text)
+        text = self.stem(text)
+
+        return text
--- task.py
+++ task.py
@@ -1,22 +1,24 @@
 # -*- coding: utf-8 -*-
-import celery
 from datetime import datetime
-from unidecode import unidecode
+
+import celery
+
 from classifier import CustomNaiveBayesAnalyzer, SentimentClassifier
 import connections
-from fetcher import get_latest_tweet, get_tweets
+from fetcher import get_latests_tweets
+from normalizer import TweetNormalizer
 
 DATABASES = ['senti_lex', 'puc_portuguese', 're_li']
 
 classifier = SentimentClassifier()
 analyzer = CustomNaiveBayesAnalyzer(databases=DATABASES)
+normalizer = TweetNormalizer('portuguese')
 
 
 @celery.task
 def search(q):
     with connections.get_db_connection() as client:
-        latest = get_latest_tweet(q)
-        tweets = get_tweets(q, latest)
+        tweets = get_latests_tweets(q)
 
         if not tweets:
             print 'There are NO new tweets'
@@ -24,7 +26,8 @@ def search(q):
             print '{0} new tweets fetched!'.format(len(tweets))
 
             for tweet in tweets:
-                sentiment = classifier.classify(tweet['text'], analyzer)
+                normalized_text = normalizer.normalize(tweet['text'])
+                sentiment = classifier.classify(normalized_text, analyzer)
 
                 # Creating the 'neutral' classification to measure the accuracy of the model
                 if sentiment.p_pos == .5:
@@ -33,6 +36,7 @@ def search(q):
                     classification = sentiment.classification
 
                 tweet['sentiment'] = {
+                    'normalized_text': normalized_text,
                     'classification': classification,
                     'p_pos': sentiment.p_pos,
                     'p_neg': sentiment.p_neg,
